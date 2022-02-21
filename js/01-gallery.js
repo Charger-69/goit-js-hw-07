63700c8bf661d868f018ad26e649fb603ea6900d
BogdanV <zarazailo@gmail.com>
2022-02-21 21:12:49 +0200

    1 fin

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,37 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryElement = document.querySelector(".gallery");
+const createImageMarkup = createGalleryItem(galleryItems);
+
+function createGalleryItem(element) {
+  return element
+    .map(({ original, preview, description }) => {
+      return `
+      <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src='${preview}'
+          data-source='${original}'
+          alt='${description}'
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
+}
+
+galleryElement.insertAdjacentHTML("afterbegin", createImageMarkup);
+galleryElement.addEventListener("click", onGallaryClick);
+
+function onGallaryClick(event) {
+  event.preventDefault()
+  const item = event.target.dataset.source
+  const instanse = basicLightbox.create(`
+  <img src ="${item}" width="800" height="650">
+  `)
+  instanse.show()
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,42 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryEl = document.querySelector(".gallery");
+
+const lighthBoxMarkup = (item) => {
+  const { description, original, preview } = item;
+
+  return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`;
+};
+
+const lightBoxGalleryInsert = galleryItems.map(lighthBoxMarkup).join("");
+
+// function createGalleryItem(element) {
+//     return element
+//         .map(({ preview, original, description }) => {
+//             return `
+//             <a class="gallery__item" href="${original}">
+//             <img class="gallery__image" src="${preview}" alt="${description}" />
+//             </a>`
+//         })
+//         .join('');
+// };
+
+const handler = (event) => {
+  event.preventDefault();
+  let gallery = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+  return gallery;
+};
+
+// const galleryHandler = new SimpleLightbox('.gallery a', { captionsData:'alt', captionDelay:250});
+// galleryHandler.on('show.simplelightbox');
+
+galleryEl.insertAdjacentHTML('beforeend', lightBoxGalleryInsert)
+galleryEl.addEventListener('click', handler)
